42a4a7fa890d2ca01d1179f3f502adbe45e5f2cb
Ryan Peek <ryan.a.peek@gmail.com>
2025-06-26 18:46:53 -0700

    minor updates

--- _targets_user.R
+++ _targets_user.R
@@ -7,12 +7,12 @@ chunk_size <- 200
 make_timelapse_video <- TRUE # or change to FALSE
 
 # Date filter for videos:
-date_start <- as.Date("2015-10-01")
-date_end <- as.Date("2016-03-01") # or as "YYYY-MM-DD"
+date_start <- as.Date("2022-10-01")
+date_end <- as.Date("2023-05-01") # or as "YYYY-MM-DD"
 
 # Time filter for videos, 24HR format
-time_start <- "11:00:00"
-time_end <- "13:00:00"
+time_start <- "12:00:00"
+time_end <- "12:30:00"
 
 # ROI MASK TYPE IF IT EXISTS
 mask_type <- "WA_01_04"
@@ -20,6 +20,6 @@ mask_type <- "WA_01_04"
 # DO NOT CHANGE MANUALLY! -----------------------------
 # these are updated automatically using `source("set_photo_dir.R")`
 # select a photo inside the folder of interest
-user_directory <- "E:/TIMELAPSE_CWS/TUO-CLAV/20170205"
+user_directory <- "E:/TIMELAPSE_DTSM/CampCreek_C04/20220930"
 exif_directory <- fs::path_dir(user_directory)
-site_id <- "TUO-CLAV"
+site_id <- "CampCreek_C04"
